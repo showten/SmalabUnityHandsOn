05322446295edbc2a871a34e27f856818a584977
showten <shoeten060522@icloud.com>
2015-09-01 03:04:21 +0900

    ハンズオン用プロジェクト

---
+++ project/Assets/AppMaster.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AppMaster : MonoBehaviour {
+
+	public string port_num ="127.0.0.1";
+	
+	public void Register(){
+		
+		InputField id = GameObject.Find("Canvas/IdField").GetComponent<InputField> ();
+		InputField name = GameObject.Find("Canvas/NameField").GetComponent<InputField> ();
+		DataBaseComunication DBC = GetComponent<DataBaseComunication> ();
+
+		int id_num = int.Parse(id.text);
+		
+		DBC.RegisterPOST(port_num,id_num,name.text); 
+		
+	}
+
+	public void Reder(){
+		
+		InputField id = GameObject.Find("Canvas/IdField").GetComponent<InputField> ();
+		DataBaseComunication DBC = GetComponent<DataBaseComunication> ();
+		
+		int id_num = int.Parse(id.text);
+		
+		DBC.RederPOST(port_num,id_num); 
+		
+	}
+
+	public void Message(string text){
+		Text name = GameObject.Find("Canvas/Name").GetComponent<Text>();
+		name.text = text;
+	}
+	
+	
+}
---
+++ project/Assets/DataBaseComunication.cs
@@ -0,0 +1,18 @@
+﻿using System; 
+using UnityEngine;
+using System.Collections; 
+using System.Collections.Generic;
+
+public class DataBaseComunication : MonoBehaviour {
+
+	public WWW RegisterPOST(string port,int login_num,string name){
+		
+	}
+
+	public WWW RederPOST(string port,int login_num){
+
+	}
+	
+	
+
+}
