cb988b792f3fd039b36747e3b9a04c27af082428
showten <shoeten060522@icloud.com>
2015-09-01 03:22:13 +0900

    ハンズオン修正版

--- project/Assets/DataBaseComunication.cs
+++ project/Assets/DataBaseComunication.cs
@@ -6,13 +6,13 @@ using System.Collections.Generic;
 public class DataBaseComunication : MonoBehaviour {
 
 	public WWW RegisterPOST(string port,int login_num,string name){
-		
+	
 	}
 
 	public WWW RederPOST(string port,int login_num){
-
+ 
 	}
 	
-	
+
 
 }
